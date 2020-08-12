c76f31ba12faeb45724a60215f7e32e920030067
俊华 蔡 <1032031912@qq.com>
2020-08-12 09:04:41 +0800

    使用嵌入窗体

--- 第十三天代码/Form1.Designer.cs
+++ 第十三天代码/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(152, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(886, 562);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 73);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "打开窗体一";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 73);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "打开窗体二";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 170);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 73);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "打开窗体三";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1043, 577);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- 第十三天代码/Form1.cs
+++ 第十三天代码/Form1.cs
@@ -15,5 +15,43 @@ namespace 第十三天代码
         {
             InitializeComponent();
         }
+
+        //关闭之前嵌入的窗体
+        private void CloseForm()
+        {
+            foreach (Control item in this.panel1.Controls)
+            {
+                if (item is Form)
+                {
+                    Form objControl = (Form)item;
+                    objControl.Close();
+                }
+            }
+        }
+        
+        private void OpenForm(Form objForm)
+        {
+            //擦除之前新打开的窗体
+            CloseForm();
+            objForm.TopLevel = false;//关闭顶级窗体
+            objForm.FormBorderStyle = FormBorderStyle.None;//去除图像边框
+            objForm.Parent = this.panel1;//指定容器
+            objForm.Dock = DockStyle.Fill;//填充满容器
+            objForm.Show();//显示新的窗体
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 fr2 = new Form2();
+            //
+            OpenForm(fr2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 fr3 = new Form3();
+            //
+            OpenForm(fr3);
+        }
     }
 }
